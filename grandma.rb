c8b60b8b33942d5bd21eb687d869abb3531a050c
Nahum <losond8@gmail.com>
2021-07-09 14:52:41 -0400

    Need to pay more attention to the specs.

--- grandma.rb
+++ grandma.rb
@@ -12,14 +12,34 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
+
     if phrase == 'I LOVE YOU GRANDMA!'
         return 'I LOVE YOU TOO PUMPKIN!'
-    end
 
-    if phrase != 'I LOVE YOU GRANDMA!'
-        return 'HUH?! SPEAK UP, SONNY!'
-    else
+    elsif phrase == phrase.upcase 
+
         return "NO, NOT SINCE 1938!"
+    else 
+        return 'HUH?! SPEAK UP, SONNY!'
     end
 end
+
+#this also works too
+# def speak_to_grandma(phrase)
+
+#     if phrase == 'I LOVE YOU GRANDMA!'
+#         return 'I LOVE YOU TOO PUMPKIN!'
+
+#     elsif phrase != phrase.upcase 
+        
+#         return 'HUH?! SPEAK UP, SONNY!'
+#     else 
+#         return "NO, NOT SINCE 1938!"
+#     end
+# end
+
+
+#NOTE: This method is taking in an arg (phrase) so we need to make sure we also pass that arg into the method as well
+
+#NOTE: specs tell us whether or not we are taking in an arg. Please pay attention brodie
